585dc32a8f675c026f9c79923aecd36c6563c5fc
biswarup16 <biswarupdaslyf@gmail.com>
2023-06-11 21:10:16 +0530

    sdf

--- project/project/settings.py
+++ project/project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-e)a*%_h$$niaqv17u8ywnubc4m#9$cpe%*8hhu2+lkta&g1#qw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
